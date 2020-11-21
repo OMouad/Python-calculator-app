7e72d9594a9c761be8e6547709b7f3dc5eeb2364
OMouad <71100063+OMouad@users.noreply.github.com>
2020-11-21 13:02:24 +0100

    displaying buttons

--- calculator.py
+++ calculator.py
@@ -32,9 +32,29 @@ button_equal = Button(root, text="=", padx=10, pady=20, command=button_func)
 button_comma = Button(root, text=".", padx=10, pady=20, command=button_func)
 button_changesign = Button(root, text="+/-", padx=10,
                            pady=20, command=button_func)
+button_clear = Button(root, text="C", padx=10, pady=20, command=button_func)
 
 
+button_0.grid(row=5, column=1)
+button_comma.grid(row=5, column=2)
 
+button_1.grid(row=4, column=0)
+button_2.grid(row=4, column=1)
+button_3.grid(row=4, column=2)
 
+button_4.grid(row=3, column=0)
+button_5.grid(row=3, column=1)
+button_6.grid(row=3, column=2)
+button_plus.grid(row=3, column=3)
+
+button_7.grid(row=2, column=0)
+button_8.grid(row=2, column=1)
+button_9.grid(row=2, column=2)
+button_minus.grid(row=2, column=3)
+
+button_clear.grid(row=1, column=0)
+button_changesign.grid(row=1, column=1)
+button_divide.grid(row=1, column=2)
+button_multiply.grid(row=1, column=3)
 
 root.mainloop()
