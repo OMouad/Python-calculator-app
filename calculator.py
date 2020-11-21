c4eecc1da9526acfafa48e6c866055ab07640e63
OMouad <71100063+OMouad@users.noreply.github.com>
2020-11-21 12:41:02 +0100

    Created the input widget

--- calculator.py
+++ calculator.py
@@ -3,3 +3,7 @@ from math import *
 
 root = Tk()
 root.title("My calculator")
+
+entry = Entry(root, width=35, borderwidth=5)
+entry.grid(row=0, column=0, columnspan=4, padx=10, pady=10)
+entry.insert(0, '0.0')
